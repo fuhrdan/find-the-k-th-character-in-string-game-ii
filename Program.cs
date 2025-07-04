11fd4f81506cb493e4d4c2fa70ff333861bed3b6
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2025-07-04 08:18:54 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿//*****************************************************************************
+//** 3307. Find the K-th Character in String Game II                leetcode **
+//*****************************************************************************
+
+char kthCharacter(long long k, int* operations, int operationsSize) {
+    int hashbrown = 0; 
+    int t;
+
+    while (k != 1)
+    {
+        t = 63 - __builtin_clzll(k); 
+
+        if ((1LL << t) == k)
+        {
+            t--;
+        }
+
+        k -= (1LL << t);
+
+        if (operations[t] != 0)
+        {
+            hashbrown++;
+        }
+    }
+
+    char retval = (char)('a' + (hashbrown % 26));
+    return retval;
+}
